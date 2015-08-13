2d2f392982868e45d38c04d45ead661bb0c7606f
Joshua Swiss <swissjoshua@gmail.com>
2015-08-13 19:46:04 +0100

    changed innerHTML to log basic calc answer

--- advanced_calc/js/main.js
+++ advanced_calc/js/main.js
@@ -1,31 +1,30 @@
-//write an event listener on the button to test
-// don't touch HTML/CSS
-//only touch DOM & JS
-//when click calc button on basic-answer, want to change hide to show
-//focus on getting the basic calc working, test the button with console.log(), then work on getting the answer to the place
-//write eventListener on button, test inputs from each button, and manipulate accordingly
-//basic, BMI, Mortgage, trip --> in order
-//are we setting calcs?
-//take <script></script> out of head and put below body
-
 var basicCalc = document.getElementById("basic-calc");
 
 
 //this should store the value
 basicCalc.addEventListener("click", function(){
+  
+  //defines variables and sets them equal to numbers
   var basicNum1 = parseFloat(document.getElementById("basic-num-1").value);
   var basicNum2 = parseFloat(document.getElementById("basic-num-2").value);
+  
   console.log(basicNum1);
   console.log(basicNum2);
   var basicOp = document.getElementById("basic-operation").value;
   console.log(basicOp);
+
+  //If statement to work the operator
     if (basicOp === "+") {
-      console.log(basicNum1 + basicNum2);
+      var ans1 = basicNum1 + basicNum2;
     } else if (basicOp === "-") {
-      console.log(basicNum1 - basicNum2);
+      var ans1 = basicNum1 - basicNum2;
     } else if (basicOp === "*") {
-      console.log(basicNum1 * basicNum2); 
+      var ans1 = basicNum1 * basicNum2; 
     } else if (basicOp === "/") {
-      console.log(basicNum1 / basicNum2);
-    };     
+      var ans1 = basicNum1 / basicNum2;
+    }; 
+
+    console.log(ans1);
+
+  document.getElementById("basic-answer-alert").innerHTML = ans1;  
 });
